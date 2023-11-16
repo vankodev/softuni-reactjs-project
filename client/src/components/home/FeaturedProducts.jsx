a9fc4e019e92c3e08a177114faa0daf734b9b498
Ivan Ivanov <vankodev@gmail.com>
2023-11-16 23:53:28 +0200

    Make Featured Products component dynamic

--- client/src/components/home/FeaturedProducts.jsx
+++ client/src/components/home/FeaturedProducts.jsx
@@ -1,14 +1,20 @@
+import { useEffect, useState } from "react";
+import * as productService from "../../services/productService";
 import ProductCard from "../product-card/ProductCard";
 
 export default function FeturedProducts() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        productService.get().then((result) => setProducts(result));
+    }, []);
+
     return (
         <div className="featuredProducts">
             <h2>Featured Products</h2>
-            <div>
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-            </div>
+            {products.slice(0, 3).map((product) => (
+                <ProductCard key={product._id} {...product} />
+            ))}
         </div>
     );
 }
