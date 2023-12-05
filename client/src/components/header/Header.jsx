7a63deac7f2960088dd451159268befc2cf1f8f1
Ivan Ivanov <vankodev@gmail.com>
2023-12-05 14:30:50 +0200

    Fixed nav to show active page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,29 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
 
 export default function Header() {
+    const location = useLocation();
+    const isActive = (path) => location.pathname === path;
+
     return (
         <div className={styles.header}>
             <h2>
-                <Link className={styles.logo} to="/">
+                <Link to="/" className={styles.logo}>
                     GamingLaptops
                 </Link>
             </h2>
             <nav className={styles.navigation}>
                 <li>
-                    <Link to="/products">Products</Link>
+                    <Link
+                        to="/products"
+                        className={isActive("/products") ? styles.active : ""}
+                    >
+                        Products
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/products/add">Add Product</Link>
+                    <Link
+                        to="/products/add"
+                        className={
+                            isActive("/products/add") ? styles.active : ""
+                        }
+                    >
+                        Add Product
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/logout">Logout</Link>
+                    <Link
+                        to="/logout"
+                        className={isActive("/logout") ? styles.active : ""}
+                    >
+                        Logout
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/login">Login</Link>
+                    <Link
+                        to="/login"
+                        className={isActive("/login") ? styles.active : ""}
+                    >
+                        Login
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/register">Register</Link>
+                    <Link
+                        to="/register"
+                        className={isActive("/register") ? styles.active : ""}
+                    >
+                        Register
+                    </Link>
                 </li>
             </nav>
         </div>
