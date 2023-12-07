177cb4c2d9aabf0e1c069b18b3076605f28035e5
Ivan Ivanov <vankodev@gmail.com>
2023-12-07 18:48:19 +0200

    Create Requester

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,21 +1,15 @@
+import * as request from "../lib/request";
+
 const baseUrl = "http://localhost:3030/jsonstore/products";
 
 export const getAll = async () => {
-    const response = await fetch(baseUrl);
-    const result = await response.json();
+    const result = await request.get(baseUrl);
+
     return Object.values(result);
 };
 
-export const create = async (productData) => {
-    const response = await fetch(baseUrl, {
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify(productData),
-    });
-
-    const result = await response.json();
+export const create = async (gameData) => {
+    const result = await request.post(baseUrl, gameData);
 
     return result;
 };
