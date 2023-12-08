0978367a0099a380a527df069508aa1a8a288f40
Ivan Ivanov <vankodev@gmail.com>
2023-12-08 17:53:27 +0200

    Create custom hook useForm

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,18 +1,36 @@
+import useForm from "../../hooks/useForm";
 import styles from "./Login.module.css";
 
 export default function Login() {
+    const { values, onChange, onSubmit } = useForm({
+        email: "",
+        password: "",
+    });
+
     return (
         <div className="container">
             <div className={styles.login}>
                 <h1 className={styles.header}>Login</h1>
-                <form className="form">
+                <form className="form" onSubmit={onSubmit}>
                     <div>
-                        <label>Username:</label>
-                        <input type="text" name="username" />
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            onChange={onChange}
+                            value={values.email}
+                        />
                     </div>
                     <div>
-                        <label>Password:</label>
-                        <input type="password" name="password" />
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            id="login-password"
+                            name="password"
+                            onChange={onChange}
+                            value={values.password}
+                        />
                     </div>
                     <button type="submit">Login</button>
                 </form>
