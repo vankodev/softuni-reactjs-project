622d9cbcfd2fbc5a069a7d6f54ad9059fa93080c
Ivan Ivanov <vankodev@gmail.com>
2023-12-09 15:42:40 +0200

    Migrate to collections from jsonstore

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,11 +1,11 @@
 import * as request from "../lib/request";
 
-const baseUrl = "http://localhost:3030/jsonstore/products";
+const baseUrl = "http://localhost:3030/data/products";
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
 
-    return Object.values(result);
+    return result;
 };
 
 export const getOne = async (productId) => {
